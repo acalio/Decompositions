249f5959926e903f1c146b46561deb3bc8fbc1e3
acalio <a.calio@dimes.unical.it>
2020-03-02 10:14:04 +0100

    init

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <networkit/io/EdgeListReader.hpp>
 #include <networkit/centrality/DegreeCentrality.hpp>
 #include <networkit/graph/BFS.hpp>
-#include <boost/program_options.hpp>
+//#include <boost/program_options.hpp>
 #include "../include/decompositions/hDegreeCentrality.hpp"
 #include "../include/decompositions/DistanceGeneralizedCoreDecomposition.hpp"
 #include <iostream>
@@ -13,17 +13,19 @@
 #include <iterator>
 
 
-namespace po = boost::program_options;
+// namespace po = boost::program_options;
 
 int main(int argc, char const *argv[])
-{
+{   
 
-    po::positional_options_description pos_args;
-    pos_args.add("input", 1);
-    pos_args.add("output", 1);
-    po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).positional(pos_args).run(), vm);
-    po::notify(vm);
+    NetworKit::Graph g;
+    
+    // po::positional_options_description pos_args;
+    // pos_args.add("input", 1);
+    // pos_args.add("output", 1);
+    // po::variables_map vm;
+    // po::store(po::command_line_parser(argc, argv).positional(pos_args).run(), vm);
+    // po::notify(vm);
     //auto data = vm["dataset"].as<std::vector<std::string>>();
     //std::cout << data[0] << std::endl; 
 
